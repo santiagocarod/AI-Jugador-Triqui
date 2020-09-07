68dee56435610e73480c4978705c2209b97bfec5
Santiago Caro <santiagocaroduque@gmail.com>
2020-09-07 18:18:22 -0500

    Usuario juega y valida. Acaba en empate o gana

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from jugadorTriqui import mejorJugada
-from util import imprimirMatriz
+from heuristica import buscarGanar
+from util import imprimirMatriz,turnoJugador,disponibles
 
 triqui= [['N' for i in range(3)]for j in range(3)]
 
@@ -7,12 +8,39 @@ triqui2=[['X','O','O'],
         ['O','X','X'],
         ['X','N','O']]
 
-triqui3=[['N','N','N'],
-        ['N','N','N'],
-        ['N','N','N']]
 
 
+ganador = False
+while(not ganador):
+	triqui = mejorJugada(triqui)
+	if(buscarGanar(triqui,'X') == 1 or disponibles(triqui)==0):
+		ganador = True
+		break
+	print("El tablero es:")
+	imprimirMatriz(triqui)
+	triquiAux = None
+	while(triquiAux == None):
+		triquiAux = turnoJugador(triqui)
+		if (triquiAux == None):
+			print("Jugada Invalida, por favor ingrese una casilla valida")
+			imprimirMatriz(triqui)
+	triqui = triquiAux
+	imprimirMatriz(triqui)
+	if(buscarGanar(triqui,'O') == 1):
+		ganador = True
+		break
 
+print()
+print()
+print("JUEGO TERMINADO!!!")
+print()
+print()
+print("TABLERO FINAL:")
+imprimirMatriz(triqui)
 
-print("MEJOR JUGADA")
-imprimirMatriz(mejorJugada(triqui2))
+if(buscarGanar(triqui,'X')==1):
+	print("EL GANADOR ES 'X' !! ")
+elif(buscarGanar(triqui,'O')==1):
+	print("EL GANADOR ES 'O' !! ")
+else:
+	print("QUE BUENA PARTIDA! ES UN EMPATE!")
--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 def imprimirMatriz(juego):
+	print("  A B C")
 	for i in range(3):
+		print(i+1, end = ' ')
 		for j in range(3):
 			if juego[i][j] !='N':
 				print(f'{juego[i][j]}', end = ' ')
@@ -19,3 +21,34 @@ def filtro(juego,jugada):
             if(juego[i][j] =='N' and jugada[i][j] =='O'):
                 jugada[i][j] ='N'
     return jugada
+
+def jugar(juego,jugada):
+	if (validarJugada(jugada)):
+		columna = ord(jugada[0])-65
+		fila = int(jugada[1])-1
+		if (juego[fila][columna] =='N'):
+			juego[fila][columna] = 'O'
+			return juego
+
+def validarJugada(jugada):
+	letrasValidas = ['A','B','C']
+	numerosValidos = ['1','2','3']
+	if (len(jugada) == 2):
+		letra = jugada[0]
+		numero = jugada[1]
+		if(letra in letrasValidas):
+			if(numero in numerosValidos):
+				return True
+	return False
+
+def disponibles(juego):
+	cuenta = 0
+	for a in juego:
+		cuenta += a.count('N')
+	return cuenta
+
+def turnoJugador(juego):
+	print("En que casilla quieres jugar? (EJ: A1)")
+	jugada = input()
+	juego = jugar(juego,jugada)
+	return juego
